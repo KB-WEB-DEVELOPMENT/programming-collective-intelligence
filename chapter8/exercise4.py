248a07df5ee422a3a09bc4d7bc9439cf6cf8e119
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-21 12:17:49 +0200

    Create exercise4.py

---
+++ chapter8/exercise4.py
@@ -0,0 +1,7 @@
+# EXERCISE 4: 
+# --> Answer: using the definition of the Gaussian weight function (p.175), if ss(=sigma
+# in the Gaussian probability distribution function) is too high, the range of weights between
+# 0 and 1 is distributed over larger distances.
+# If ss is too low, the opposite is true (i.e: the Gaussian probability curve "falls" faster)  
+#
+
