084f6c09a49fd5a71301b2a3ba26daff00fc1f9d
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-21 12:18:08 +0200

    Create exercise5.py

---
+++ chapter8/exercise5.py
@@ -0,0 +1,5 @@
+# EXERCISE 5:
+# Answer: Looking at the (1) attributes, (2) price, (3) bids and (4) feedback
+# numerical values returned by the getItem method on the argument laptop[7][0] (p.194),
+# I suspect that the most important variables are the ones contained in (1). The others 
+# (2), (3) and (4) would have to be scaled.
