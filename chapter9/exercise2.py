75dcf6795bea2d3d6cc3bb39d4fc8745df3480e4
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-21 12:21:11 +0200

    Create exercise2.py

---
+++ chapter9/exercise2.py
@@ -0,0 +1,6 @@
+# EXERCISE 2: Referring to the matchmaker.csv file sample on p. 198,
+# we can create a cost function as follows:
+# costfunction = A+B+C+D
+# where: A = abs(age1-age2), B += 1 for each man's hobby != woman's hobby , B=0 if same list of hobbies 
+# C = 0 if home city 1 = home city 2, C = 1 if home city 1 != home city 2
+# D = abs(number of children wished by the man - number of children wished by the woman)
