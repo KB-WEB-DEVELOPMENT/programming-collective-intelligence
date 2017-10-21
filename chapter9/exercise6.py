13bc4ba88b4c0f78ea47945c5a4189db05cb1d28
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-21 12:28:52 +0200

    Create exercise6.py

---
+++ chapter9/exercise6.py
@@ -0,0 +1,4 @@
+# EXERCISE 6: basicallly modify list of fields in 
+# function getinfo(self, users) on p.222
+# additional infos here: https://developers.facebook.com/docs/graph-api/reference/user/movies/
+
