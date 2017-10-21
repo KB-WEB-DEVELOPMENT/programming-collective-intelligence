76466fe123c11d5a1a41c37d5697803772e22f41
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-21 11:58:53 +0200

    Create exercise2.py

---
+++ chapter6/exercise2.py
@@ -0,0 +1,18 @@
+# EXERCISE 2: 
+# Pr(Document) - Probability that given a randomly selected category,
+#                the document in question has a features count
+#                greater than 0 for that category.
+# Pr(Document) = (A)/(B) where:
+# (A): Total number of categories whose features count is > 0 for that document
+# (B): Total number of categories alltogether
+
+def docprob(self,f,cat)
+# total number of categories alltogether
+  denominator=len(categories(self))
+# total number of categories whose features count >  0 for that document
+  categories_count=0
+    if f in self.fc and cat in self.fc[f]:
+	   categories_count+=1
+	numerator=categories_count
+	
+  return float(numerator/denominator)
