93a199f0cb328df99ba19d453315f7d1250d8210
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-21 12:52:33 +0200

    Create exercise5.py

---
+++ chapter11/exercise5.py
@@ -0,0 +1,5 @@
+# EXERCISE 5: The more variables, the more difficulties and the longer 
+# it would  take for the function to be detected (example below)
+
+def hiddenfunction(v,w,x,y,z):
+  return v**5 + 2*w**4 + 3*x**3 + 4*y**2 + 5*z
