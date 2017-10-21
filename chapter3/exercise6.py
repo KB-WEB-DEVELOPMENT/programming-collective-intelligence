26d5817fa52da4f95d6bf6ac6b0df72c36371a80
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-21 11:43:49 +0200

    Create exercise6.py

---
+++ chapter3/exercise6.py
@@ -0,0 +1,9 @@
+# EXERCISE 6 
+
+# 1) Assuming the k centroids are spatially randomly distributed at the start and the number 
+# of items remains constant for each experiment, the total distance must decrease as k grows for
+# a large number x of iterations.
+# I suspect the relationship between k and the total distance is not (inversely related) linear, but most likely closer to an
+# (inversely related) exponential function.
+
+#2) At some point (?), adding more k does not change the total distance significantly.
