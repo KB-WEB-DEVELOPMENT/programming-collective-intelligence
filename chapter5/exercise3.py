230ee52ad089ba0c4d0aab30699b4eebc88640c3
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-27 20:23:02 +0200

    Update exercise3.py

--- chapter5/exercise3.py
+++ chapter5/exercise3.py
@@ -3,17 +3,17 @@
 def geneticoptimize(domain,costf,popsize=50,step=1,mutprod=0.2,elite=0.2,maxiter=100):
 
   # Mutation Operation
-  def mutate(vec):
-    i=random.randint(0,len(domain)-1)
-    if random.random( )<0.5 and vec[i]>domain[i][0]:
-      return vec[0:i]+[vec[i]-step]+vec[i+1:]
-    elif vec[i]<domain[i][1]:
-      return vec[0:i]+[vec[i]+step]+vec[i+1:]
+def mutate(vec):
+  i=random.randint(0,len(domain)-1)
+  if random.random( )<0.5 and vec[i]>domain[i][0]:
+    return vec[0:i]+[vec[i]-step]+vec[i+1:]
+  elif vec[i]<domain[i][1]:
+    return vec[0:i]+[vec[i]+step]+vec[i+1:]
 
    # Crossover Operation
-   def crossover(r1,r2):
-     i=random.randint(1,len(domain)-2)
-     return r1[0:i]+r2[i:]
+ def crossover(r1,r2):
+   i=random.randint(1,len(domain)-2)
+   return r1[0:i]+r2[i:]
 
    # Build the initial population
    pop=[]
@@ -27,30 +27,30 @@ def geneticoptimize(domain,costf,popsize=50,step=1,mutprod=0.2,elite=0.2,maxiter
    # Main loop
    for i in range(maxiter):
    
-       scores=[(costf(v),v) for v in pop]       
+     scores=[(costf(v),v) for v in pop]       
 	   
-       if i % 10 == 0 and scores[i][i] > scores[0][0]:	   
-          break   
+     if i % 10 == 0 and scores[i][i] > scores[0][0]:	   
+        break   
 	   
-       else:
-	   scores.sort( )	   
-	   ranked=[v for (s,v) in scores]
+     else:
+       scores.sort( )	   
+       ranked=[v for (s,v) in scores]
 
-       # Start with the pure winners
-       pop=ranked[0:topelite]
+   # Start with the pure winners
+   pop=ranked[0:topelite]
 
-       # Add mutated and bred forms of the winners
-       while len(pop)<popsize:
-         if random.random( )<mutprob:
-            # Mutation
-            c=random.randint(0,topelite)
-            pop.append(mutate(ranked[c]))
-         else:
-           # Crossover
-           c1=random.randint(0,topelite)
-           c2=random.randint(0,topelite)
-           pop.append(crossover(ranked[c1],ranked[c2]))
+   # Add mutated and bred forms of the winners
+   while len(pop)<popsize:
+     if random.random( )<mutprob:
+     # Mutation
+       c=random.randint(0,topelite)
+       pop.append(mutate(ranked[c]))
+     else:
+        # Crossover
+        c1=random.randint(0,topelite)
+        c2=random.randint(0,topelite)
+        pop.append(crossover(ranked[c1],ranked[c2]))
 	   
-      # Print current best score
-      print scores[0][0]
+     # Print current best score
+     print scores[0][0]
    return scores[0][1]
