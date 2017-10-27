a35fd6ded83b4826c6eaea8a57740fae02ec5cac
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-27 20:11:35 +0200

    Update exercise4.py

--- chapter11/exercise4.py
+++ chapter11/exercise4.py
@@ -2,7 +2,7 @@
 
 def evolve(pc,popsize,rankfunction,maxgen=500,mutationrate=0.1,breedingrate=0.4,pexp=0.7,pnew=0.05,noimprovementitervar=20):
 
-  def selectindex():
+def selectindex():
     
    j=0
    maxscore=10e10
@@ -10,10 +10,8 @@ def evolve(pc,popsize,rankfunction,maxgen=500,mutationrate=0.1,breedingrate=0.4,
    return int(log(random())/log(pexp))
    population=[makerandomtree(pc) for i in range(popsize)]
 
-   for i in range(maxgen):
-	
-     while j<=noimprovementitervar:
-	  
+   for i in range(maxgen):	
+     while j<=noimprovementitervar:	  
       scores=rankfunction(population)	  
         
       if scores[0][0]<maxscore:
@@ -25,18 +23,17 @@ def evolve(pc,popsize,rankfunction,maxgen=500,mutationrate=0.1,breedingrate=0.4,
 	  
       newpop=[scores[0][1],scores[1][1]]
 
-
     while len(newpop)<popsize:
       if random()>pnew:
-	    newpop.append(mutate(
-		crossover(scores[selectindex()][1],
-		scores[selectindex( )][1],
-		probswap=breedingrate),
-        pc,probchange=mutationrate))
+	 newpop.append(mutate(
+	    crossover(scores[selectindex()][1],
+	    scores[selectindex( )][1],
+	    probswap=breedingrate),
+           pc,probchange=mutationrate))
       else:
         newpop.append(makerandomtree(pc))
       
     population=newpop
     
-  scores[0][1].display( )
+  scores[0][1].display()
   return scores[0][1]
