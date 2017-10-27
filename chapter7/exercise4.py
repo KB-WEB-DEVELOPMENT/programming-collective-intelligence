b612a17dd83d17d1a7837d957467dec3e8901f5a
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-27 20:35:19 +0200

    Update exercise4.py

--- chapter7/exercise4.py
+++ chapter7/exercise4.py
@@ -10,16 +10,20 @@ def buildtree(rows,scoref=entropy):
   
   column_count=len(rows[0])-1
   for col in range(0,column_count):
+	
     # Generate the list of different values in
     # this column
+	
     column_values={}
     for row in rows:
       column_values[row[col]]=1
+
     # Now try dividing the rows up for each value
     # in this column
-    for value in column_values.keys( ):
+
+    for value in column_values.keys( ):	
+       if value=None: value="MISSING DATA"
 	
-       if value=None: value="MISSING DATA"	
     (set1,set2)=divideset(rows,col,value)
 	  
       # Information gain
@@ -51,5 +55,5 @@ def buildtree(rows,scoref=entropy):
       falseBranch=buildtree(best_sets[0])
       falseBranch+=buildtree(best_sets[1])
 	  
-  return decisionnode(col=best_criteria[0],value=best_criteria[1],tb=trueBranch,fb=falseBranch)
+      return decisionnode(col=best_criteria[0],value=best_criteria[1],tb=trueBranch,fb=falseBranch)
 	
