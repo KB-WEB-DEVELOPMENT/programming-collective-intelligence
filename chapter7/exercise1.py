af4227d51f7d4eb834563d5f7178e54550641320
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-27 19:17:40 +0200

    Update exercise1.py

--- chapter7/exercise1.py
+++ chapter7/exercise1.py
@@ -7,9 +7,9 @@ def classify(observation,tree):
   total=0
   if tree.results!=None:
      for count in tree.results.values():
-	   total+=count
-	 for service,count in tree.results.items():
-	    probcats[service]=float(count)/total	 
+       total+=count
+     for service,count in tree.results.items():
+       probcats[service]=float(count)/total	 
   else:
     v=observation[tree.col]
     branch=None
@@ -27,29 +27,30 @@ def mdclassify(observation,tree):
   probcats={}
   total=0
   if tree.results!=None:
-	 for count in tree.results.values():
-	   total+=count
-	 for service,count in tree.results.items():
-	    probcats[service]=float(count)/total  
+    for count in tree.results.values(): 
+      total+=count
+    for service,count in tree.results.items():
+      probcats[service]=float(count)/total  
   else:
     v=observation[tree.col]
     if v==None:
-		tr,fr=mdclassify(observation,tree.tb),mdclassify(observation,tree.fb)
-		tcount=sum(tr.values( ))
-		fcount=sum(fr.values( ))
-		tw=float(tcount)/(tcount+fcount)
-		fw=float(fcount)/(tcount+fcount)
-        result={}
-		for k,v in tr.items( ): result[k]=v*tw
-		for k,v in fr.items( ): result[k]=v*fw
-		return result
-	else:
-	  if isinstance(v,int) or isinstance(v,float):
-		if v>=tree.value: branch=tree.tb
-		else: branch=tree.fb
-	else:
-	  if v==tree.value: branch=tree.tb
-      else: branch=tree.fb
+       tr,fr=mdclassify(observation,tree.tb),mdclassify(observation,tree.fb)
+       tcount=sum(tr.values( ))
+       fcount=sum(fr.values( ))
+       tw=float(tcount)/(tcount+fcount)
+       fw=float(fcount)/(tcount+fcount)
+       result={}
+		
+	for k,v in tr.items( ): result[k]=v*tw
+	for k,v in fr.items( ): result[k]=v*fw
+	return result
+    else:
+      if isinstance(v,int) or isinstance(v,float):
+         if v>=tree.value: branch=tree.tb
+	 else: branch=tree.fb
+      else:
+        if v==tree.value: branch=tree.tb
+        else: branch=tree.fb
     return mdclassify(observation,branch)
   
     return probcats  
