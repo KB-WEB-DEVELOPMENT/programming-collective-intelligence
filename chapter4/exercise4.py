8491587e92c853730bc3d1d30bd7b2490545780f
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-27 20:13:39 +0200

    Update exercise4.py

--- chapter4/exercise4.py
+++ chapter4/exercise4.py
@@ -5,8 +5,8 @@ def maxlengthdocscore(self,rows)
   maxdocsize=dict([(row[0],1000000) for row in rows])
   for row in rows:
     maxsize=max((row[i]) for i in range(2,len(row)))
-	if maxsize>maxdocsize[row[0]]:maxdocsize[row[0]]=maxsize
-  #choice:higher score means longer document,i.e: "bigger is better"
+    if maxsize>maxdocsize[row[0]]:maxdocsize[row[0]]=maxsize
+    #choice:higher score means longer document,i.e: "bigger is better"
   return self.normalizescores(maxdocsize)
   
 #weighting function to give preference to shorter documents
@@ -14,6 +14,6 @@ def minlengthdocscore(self,rows)
   mindocsize=dict([(row[0],1000000) for row in rows])
   for row in rows:
     minsize=min((row[i]) for i in range(2,len(row)))
-	if minsize<mindocsize[row[0]]:mindocsize[row[0]]=minsize
+    if minsize<mindocsize[row[0]]:mindocsize[row[0]]=minsize
   #choice:lower score means shorter document,i.e: "smaller is better"
   return self.normalizescores(mindocsize,smallisBetter=1) 
