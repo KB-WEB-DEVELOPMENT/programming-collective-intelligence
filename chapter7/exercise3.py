ac0510a6b80061a352ebe65c7f4c93f8bf32939b
Kami Barut-Wanayo <kamibarut@yahoo.com>
2017-10-27 20:33:05 +0200

    Update exercise3.py

--- chapter7/exercise3.py
+++ chapter7/exercise3.py
@@ -18,20 +18,20 @@ def buildtree(rows,scoref=entropy,min_gain=0):
     # Now try dividing the rows up for each value
     # in this column
     for value in column_values.keys( ):
-     (set1,set2)=divideset(rows,col,value)
+      (set1,set2)=divideset(rows,col,value)
 
-	 # Information gain
+     # Information gain
      p=float(len(set1))/len(rows)
      gain=current_score-p*scoref(set1)-(1-p)*scoref(set2)
      if gain>best_gain and len(set1)>0 and len(set2)>0:
         best_gain=gain
         best_criteria=(col,value)
         best_sets=(set1,set2)
-   # Create the subbranches
-   if best_gain>min_gain:
-      trueBranch=buildtree(best_sets[0])
-      falseBranch=buildtree(best_sets[1])
-      return decisionnode(col=best_criteria[0],value=best_criteria[1],tb=trueBranch,fb=falseBranch)
+     # Create the subbranches
+     if best_gain>min_gain:
+       trueBranch=buildtree(best_sets[0])
+       falseBranch=buildtree(best_sets[1])
+       return decisionnode(col=best_criteria[0],value=best_criteria[1],tb=trueBranch,fb=falseBranch)
     
-	else:
-      return decisionnode(results=uniquecounts(rows))
+     else:
+       return decisionnode(results=uniquecounts(rows))
